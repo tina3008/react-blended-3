a5ce241e1f3bf502cd07d80278fad4c23638ac49
tina3008 <kicja_3@ukr.net>
2024-06-01 11:32:47 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,34 @@
-import { Container, CountryList,  Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
 import { useEffect, useState } from 'react';
 import { getCountries } from 'service/countryApi';
 
 export const Home = () => {
   const [coutries, setCountries] = useState([]);
-  useEffect(
-    () => {
-      async function fetchCountry() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false)
+  useEffect(() => {
+    async function fetchCountry() {
+      setLoading(true)
+      setError(false)
+      try {
         const data = await getCountries();
-      setCountries(data)
-      
+        setCountries(data);
+      } catch (error) {
+      setError(error.massage)
       }
-      fetchCountry();
+      finally { 
+        setLoading(false)
+        
+      }
     }
-  ,[])
+    fetchCountry();
+  }, []);
   return (
     <Section>
       <Container>
+        {loading && <Loader />}
+        {error && <Heading title={ error} top/>}
         <CountryList list={coutries} />
-       
       </Container>
     </Section>
   );
